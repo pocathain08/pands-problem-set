21b68809c3fcdcf6f802a426773053d59ca4f5bc
Peadar O Cathain <peadarocathain@gmail.com>
2019-02-17 20:26:26 +0000

    Value of range

--- solution-1.py
+++ solution-1.py
@@ -1,6 +1,13 @@
 # Solution to Problem 1
 x = int (input ("Please enter a positive integer: "))
+for i in range (x):
+    print (i, i+1)
+     #   sum (i)
+#print [i]
 
-if type (x) != int:
-    print ("This is not a positive integer")
+
+
+
+#if type (x) != int:
+ #   print ("This is not a positive integer")
     
