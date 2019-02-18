2ed91ae5039ff2660e99358e1a2ed1bd3baed0fb
Peadar O Cathain <peadarocathain@gmail.com>
2019-02-18 19:28:46 +0000

    Sums upto x

--- solution-1.py
+++ solution-1.py
@@ -1,7 +1,11 @@
 # Solution to Problem 1
 x = int (input ("Please enter a positive integer: "))
-for i in range (x):
-    print (i, i+1)
+sum = 0
+for i in range (0, x+1):
+    sum = sum + i 
+print ("sum of first", x, "numbers is: ", sum) 
+
+#(i, i+1)
      #   sum (i)
 #print [i]
 
