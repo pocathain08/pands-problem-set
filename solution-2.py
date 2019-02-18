f8988cff822fc9e8979e04d21ea05ee3adb2e381
Peadar O Cathain <peadarocathain@gmail.com>
2019-02-18 20:04:18 +0000

    Previous EG

--- solution-2.py
+++ solution-2.py
@@ -1,7 +1,13 @@
 #Write a program that outputs whether or not 
 #today is a day that begins with the letter T
 
-l= (import datetime):
-    Print (l)
-    
+import datetime
+
+if datetime.datetime.today().weekday() == 1:
+    print("Yes - today begins with a T")
+
+else:
+    print ("No - today does not begin with a T.")
+
+
 
