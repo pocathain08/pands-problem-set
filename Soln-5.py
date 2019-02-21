464d9c47672125d9cc22ce968a9ca1d55aa24d81
Peadar O Cathain <peadarocathain@gmail.com>
2019-02-21 21:09:23 +0000

    This needs to loop

--- Soln-5.py
+++ Soln-5.py
@@ -3,7 +3,11 @@ x = int(input ("Please enter a positive integer: "))
 
 LCD = (((x)//2)+2)
 
-LCD_range = list(range(0, LCD))
+#LCD_range = list(range(0, LCD))
+
+for i in (LCD):
+    if x % i != 0:
+        print ("this is a prime number" )
 
 
-print (LCD_range)
+#print (LCD_range)
