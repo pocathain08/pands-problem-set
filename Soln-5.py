be6eb63613afc44864f42294d0d8b7384be7fca5
Peadar O Cathain <peadarocathain@gmail.com>
2019-02-23 20:06:59 +0000

    Prints this is not prime for each condition

--- Soln-5.py
+++ Soln-5.py
@@ -1,13 +1,11 @@
 #python primes.py
-x = int(input ("Please enter a positive integer: "))
+x = int(input("Please enter a positive integer: "))
 
 LCD = (((x)//2)+2)
+LCD_range = list(range(2, LCD))
 
-#LCD_range = list(range(0, LCD))
-
-for i in (LCD):
-    if x % i != 0:
-        print ("this is a prime number" )
-
-
+for i in (LCD_range):
+        if x % i != 0:
+                print ("this is a prime number" )
+        
 #print (LCD_range)
