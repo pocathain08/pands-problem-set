bbc81c3877d954f60f2cc8fdf2bb75a0dbfcec3b
Peadar O Cathain <peadarocathain@gmail.com>
2019-02-25 20:52:26 +0000

    Using mod == rather than !=

--- Soln-5.py
+++ Soln-5.py
@@ -1,17 +1,18 @@
 #python primes.py
 x = int(input("Please enter a positive integer: "))
 
-LCD = (((x)//2)+2)
-LCD_range = list(range(2, LCD))
+y = (((x)//2)+2)
+#LCD_range = list(range(2, LCD))
 
-for i in (LCD_range):
-        if x % i != 0:
-                print (x,"is a prime number" )
+for i in range(2, y):
+        if x % i == 0:
+                print (x,"is not a prime number" )
 
                 break
 # Python tutorial 4.4       
 else: 
-        print (x, "is not a prime number" )
+        print (x, "is a prime number" )
+
 
                 
  
