5a3e5da34d3e41bd40a540b0bba83aa599aac411
Peadar O Cathain <peadarocathain@gmail.com>
2019-02-25 21:34:02 +0000

    Draft 2

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 Soln 4 
     This program asks the user to input any positive integer and outputs the successive values of the following calculation. At each step calculate the next value by taking the current value and,if it is even, divide it by two,but if it is odd,multiply it by three and add one.
+    So, let x = inputed positive integer.
+    if x even, //2, if x odd *3 + 1. 
+    Then take this number and loop this operation (i+1)
--- Soln-4.py
+++ Soln-4.py
@@ -4,16 +4,23 @@
 
 x = int(input("Please enter a positive integer: "))
 
-New_list = x
+#New_list = x
 
-while x > 1:
-        for i in range(1, x+1):
-                if i % 2 == 0:
+#while x > 1:
+        #New_list = New_list + i
         
+        #for i in range(1, x+1):
+if x % 2 == 0:
+        New_list = (x//2)      
 
-                elif i % 2 !=0:
+elif x % 2 !=0:
+         New_list = ((x*3)+1)
+
+Print (New_list)
+         
+#Print (New_list)
                         
-        i= i-1
+        #i= i-1
 
 
         
