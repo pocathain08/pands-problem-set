d5db2a3bb27cdacdc4dcd5a6b1c5df9580db9ae1
Peadar O Cathain <peadarocathain@gmail.com>
2019-03-12 21:00:29 +0000

    Input a sentence return an indexed letter

---
+++ Soln-6.py
@@ -0,0 +1,15 @@
+#This program takes a user input string and outputs 
+#every second word. 12 Mar 2019
+
+#Please enter a sentence:
+my_str = (input("Please enter a sentence: "))
+print (my_str[0])
+
+# Python tutorial 3.1.2 Strings
+#(var1 = 'Hello World!'
+#var2 = "Python Programming"
+
+#print ("var1[0]: ", var1[0])
+#print ("var2[1:5]: ", var2[1:5]))
+
+
