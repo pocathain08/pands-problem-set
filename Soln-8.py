49516b93046f119f03c926b85828d4e859509473
Peadar O Cathain <peadarocathain@gmail.com>
2019-03-26 20:44:51 +0000

    Soln 9, finished

--- Soln-8.py
+++ Soln-8.py
@@ -3,6 +3,21 @@
 
 import datetime
 
+day = (datetime.datetime.now().strftime(' %d'))
+if day == 1:
+    Ord = "st"
+if day == 2:
+    Ord = "nd"
+if day == 3:
+    Ord = "rd"
+else:
+    Ord = "th"
+
 #https://stackoverflow.com/q/415511
-x=(datetime.datetime.now().strftime('%A, %B, %d, %Y, %H:%M:'))
+#https://stackoverflow.com/questions/1759455/how-can-i-account-for-period-am-pm-with-datetime-strptime
+x=(datetime.datetime.now().strftime(f'%A, %B %d{Ord} %Y, at %I:%M %p'))
+#Account for 1st, 2nd, 3rd, etc using an f string
+
 print (x)
+
+
