899b8e6b1478e82982eeff0d547b0e86c1909537
Peadar O Cathain <peadarocathain@gmail.com>
2019-03-26 21:33:42 +0000

    Soln 10, prints fn

--- Soln-10.py
+++ Soln-10.py
@@ -1,2 +1,18 @@
 #This program displays a plot of the functions x, x^2 and 2^x in the range [0,4].
 #Peadar O Cathain 26 Mar 2019
+
+#https://stackoverflow.com/questions/22476854/plotting-function-from-python
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+for x in range(5):
+    a = x
+    b = x**2
+    c = 2**x
+
+    y = (a, b, c)
+
+    print (y)
+
+
--- Soln-9.py
+++ Soln-9.py
@@ -1,3 +1,5 @@
 #This program reads in a text ﬁle and outputs every second ine. 
 #Theprogram should take the ﬁlename from an argument on the command line.
 #Peadar O Cathain 26 Mar 2019
+
+
