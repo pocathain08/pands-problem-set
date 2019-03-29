a27efc0efcb04cda43339c0e18dc2b76ebf9f200
Peadar O Cathain <peadarocathain@gmail.com>
2019-03-29 16:50:02 +0000

    Soln 9, Est draft 1 with Ch 1 MD.txt

--- Soln-9.py
+++ Soln-9.py
@@ -3,6 +3,13 @@
 #Peadar O Cathain 26 Mar 2019
 #Ref Section 7.2 in Python Tutorial
 
+
+
+#Ref:https://stackoverflow.com/questions/47062493/how-can-i-get-python-to-read-every-nth-line-of-a-txt-file
+with open("file.txt", "r") as f:
+    for line in f.read().split("\n")[::2]:
+        print(line)
+
 f = open('myfile.txt', 'r') #opens file in same directory, ../myfile.txt Can use abs or rel paths to access
 #Tend to keep file in same location. Note w overwrites files. \n =go to a new line
 
