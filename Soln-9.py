4c67e0b3b6c6693d93f77b4a547db7a1ab1a00f0
Peadar O Cathain <peadarocathain@gmail.com>
2019-03-30 07:33:36 +0000

    Soln 9 2nd draft

--- Soln-9.py
+++ Soln-9.py
@@ -5,18 +5,24 @@
 
 import sys
 
-with open("Moby Dick.txt", "r") as f:
+x = (sys.argv[1:])
+print (x)
+if len(x) !=1:
+    print ("Please enter title as one word")
+
+if x == ("Moby_Dick"):
+    #with open("Moby Dick.txt", "r") as f:
     
-    print("Title;", sys.argv[1])
+    print("Title;", sys.argv[1:])
 #This will print the second arguement from the Command line
 #using sys.argv[1:] will print everything bar the filename (Soln-9.py)
 #Ref: Python Tutorial 7.2
 #with open("Moby Dick.txt", "r") as f: #opens file in same directory
 
 #Ref:https://stackoverflow.com/questions/47062493/how-can-i-get-python-to-read-every-nth-line-of-a-txt-file
-    for line in f.read().split("\n")[::2]:
+    #for line in f.read().split("\n")[::2]:
     #\n =go to a new line, [::2] every second line
-        print(line)
+        #print(line)
 
 #f = open('myfile.txt', 'r') , ../myfile.txt Can use abs or rel paths to access
 #Tend to keep file in same location. Note w overwrites files. 
