8abeb390afa8d13203b5ebad96b319d57ba4c610
Peadar O Cathain <peadarocathain@gmail.com>
2019-03-31 11:58:13 +0100

    Soln 9, Finished

--- Soln-9.py
+++ Soln-9.py
@@ -6,40 +6,23 @@
 import sys
 
 for arg in sys.argv[1:]:
+    #This looks at all arguements in the command line after the file name
     if arg == ("Moby Dick"):
+        #This assumes the unser wants to look at Moby Dick specifically
+        #It then runs the program that outputs sever second line.
         with open("Moby Dick.txt", "r") as f:
+            #Opens the file and reads it. The read file is abbreviated as f
             for line in f.read().split("\n")[::2]:
+                #split("\n") splits the file into lines
+                #[:: 2] looks at every second line. 
+                #The print command outputs every second line
                 print(line)
                 f.close()
 
     elif arg != ("Moby Dick"):
+        # Prints the b\m if title is not available.
         print("That title is not available or should be entered in Quotation Marks.")
                 
-                
-
-#This will print the second arguement from the Command line
-#using sys.argv[1:] will print everything bar the filename (Soln-9.py)
-#Ref: Python Tutorial 7.2
-
-#if len(x) !=1:
-#    print ("Please enter title as one word, seperate words with _")
-
-#if x == ("Moby_Dick"):
-#    with open("Moby Dick.txt", "r") as f: 
-    #opens the file as f, if f is in same directory.
-
-#Ref:https://stackoverflow.com/questions/47062493/how-can-i-get-python-to-read-every-nth-line-of-a-txt-file
- #       for line in f.read().split("\n")[::2]:
-    #\n =go to a new line, [::2] every second line
-            
-            
-
-#f = open('myfile.txt', 'r') , ../myfile.txt Can use abs or rel paths to access
-#Tend to keep file in same location. Note w overwrites files. 
-
-#s = f.read() #reads file
-
-#f.close() #Closes the file
 
 
 
