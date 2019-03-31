6e4df1fbaf319433e4018c5ce12f6ea010a0a17f
Peadar O Cathain <peadarocathain@gmail.com>
2019-03-31 11:50:05 +0100

    Soln 6 W draft

--- Soln-6.py
+++ Soln-6.py
@@ -1,36 +1,23 @@
 #This program takes a user input string and outputs 
-#every second word. 12 Mar 2019
+#every second word. 
+#Peadar O Cathain 12 Mar 2019
 
 #Please enter a sentence: 
 #The quick brown fox jumps over the lazy dog
  
 s = (input("Please enter a sentence: "))
 
-
-
 #https://stackoverflow.com/questions/6181763/converting-a-string-to-a-list-of-words
 ssplit = s.split()
-#print (ssplit)
-#https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/
-#i is the numerical assignment of the words in the sentence.
+#ssplit is a new variable that breaks the larger string, s,
+# into smaller chunks.
+
+#https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways
 for i in range(len(ssplit)):
-#select every second word by picking out the odd words
-    if i % 2 == 1:
+#i is the numerical assignment of the words in the sentence.
+    
+    if i % 2 != 1:
+    #selects every second word by picking out the odd words    
+        print (ssplit[i])
         
-        
-        #print(x)
-        l = (ssplit[i])
 
-        print (l)
-        #print (ssplit[i])
-    #if i %2 != 1:
-     #   print (i)
-
-# Python tutorial 3.1.2 Strings
-#(var1 = 'Hello World!'
-#var2 = "Python Programming"
-
-#print ("var1[0]: ", var1[0])
-#print ("var2[1:5]: ", var2[1:5]))
-
-#
