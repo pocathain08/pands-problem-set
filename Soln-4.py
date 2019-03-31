eb95a4d659fdb853e8e61e153f5ffe9ea03366f6
Peadar O Cathain <peadarocathain@gmail.com>
2019-03-31 11:20:46 +0100

    Soln 4, Finished

--- Soln-4.py
+++ Soln-4.py
@@ -1,9 +1,14 @@
-# python collatz 25 Feb 2019
-#
+# This program asks the user to input any positive integer and 
+#outputs the successive values of the following calculation. 
+#At each step calculate the next value by taking the current value 
+#and, if it is even, divide it by two, but if it is odd, multiply 
+#it by three and add one. 
+#Peadar O Cathain 25 Feb 2019
 
 while True:
     try:
         x = int (input ("Please enter a positive integer: "))
+        # Ask the used to enter an integer
 
 # If a non-integer is entered by the user
     except ValueError:
@@ -19,26 +24,31 @@ while True:
     
     else:
         break
+        #Breaks out of the loop and goes to the next line of the program.
 
 Soln = list()
-
+#Creates an empty list. 
+#This will be used for the calculated values of x
 while x > 1:
+# The following loops will continue running until x > 1.
+# Once x in the program reached 2, it will be divided by two, 
+#this will return one and the while condition will not be true. 
+    Soln.append(x)
+    # Starts the list
         
     if x % 2 == 0:
-        print (x // 2)
-
-        #(x // 2) = Soln
-        #x += (Soln)
-                #
+    # for x even. (x mod 2  doen't leave a remainder)
         x = x//2
+        # The next value of x
 
     elif x % 2 != 0:
-        print ((3 * x) + 1)
-        #((3 * x) + 1) = (Soln)
-                #
+     # for x odd. (x mod 2 leaves a remainder)
         x = ((3 * x) +1)
+        # The next value of x
+Soln.append(x)
+# Itterates through the answere and puts them into a list called Soln
 
-        #print (Soln)
+print (Soln)
                 
 
 
